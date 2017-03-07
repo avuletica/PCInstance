decc1074be988d319e7c10d474062e7a7d88c75c
Ante <vuletic23@gmail.com>
2017-03-07 16:47:14 +0100

    Configure angular 2 directory structure

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,14 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule }  from '@angular/platform-browser';
+import { NgModule }             from '@angular/core';
+import { BrowserModule }        from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { AppComponent }         from './app.component';
+import { HomeComponent } from './components/home/home.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   bootstrap: [ AppComponent ]
 })
