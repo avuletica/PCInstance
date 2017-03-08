94bb2c0efef552c263e967cd8d093b789f8ac2de
Ante <vuletic23@gmail.com>
2017-03-08 19:24:14 +0100

    Implement angular routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,13 +1,19 @@
 import { NgModule }             from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent }           from './app.component';
+import { HomeComponent }          from './components/home/home.component';
+import { RegistrationComponent }  from './components/registration/registration.component';
+import { LoginComponent }         from './components/login/login.component';
+import { CartComponent }          from './components/cart/cart.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  { path: 'home',  component: HomeComponent },  
+  { path: 'home',  component: HomeComponent }, 
+  { path: 'cart',  component: CartComponent }, 
+  { path: 'registration',  component: RegistrationComponent }, 
+  { path: 'login',  component: LoginComponent },  
 ];
 
 @NgModule({
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,15 @@
-import { NgModule }             from '@angular/core';
-import { BrowserModule }        from '@angular/platform-browser';
+import { NgModule }               from '@angular/core';
+import { BrowserModule }          from '@angular/platform-browser';
 
-import { AppComponent }         from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { AppComponent }           from './app.component';
+import { NavigationComponent }    from './components/navigation/navigation.component';
+import { HomeComponent }          from './components/home/home.component';
+import { FooterComponent }        from './components/footer/footer.component';
+import { RegistrationComponent }  from './components/registration/registration.component';
+import { LoginComponent }         from './components/login/login.component';
+import { CartComponent }          from './components/cart/cart.component';
 
-import { AppRoutingModule }     from './app-routing.module';
+import { AppRoutingModule }       from './app-routing.module';
 
 @NgModule({
   imports: [
@@ -13,7 +18,12 @@ import { AppRoutingModule }     from './app-routing.module';
   ],
   declarations: [
     AppComponent,
-    HomeComponent
+    NavigationComponent,
+    HomeComponent,
+    FooterComponent,
+    RegistrationComponent,
+    LoginComponent,
+    CartComponent
   ],
   bootstrap: [ AppComponent ]
 })
